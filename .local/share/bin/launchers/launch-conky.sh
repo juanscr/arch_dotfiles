8b59bfc8a3e70fddf4cf47f938dba6613fafbcf4
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2023-09-02 16:29:01 +0000

    Simplify conky configuration.

--- .local/share/bin/launchers/launch-conky.sh
+++ .local/share/bin/launchers/launch-conky.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
 killall conky
-num_monitors=$("$HOME"/.config/qtile/check_number_of_monitors.sh)
-coordinate=$(xrandr --query |
-             grep primary |
-             grep -o [[:digit:]]\\++[[:digit:]]\\+\\s |
-             awk -F+ '{ print $1 }')
-
-# Spawn conky according to layout
-if [ "$num_monitors" == 2 ] && [ "$coordinate" == 0 ]; then
-    conky &
-elif [ "$num_monitors" == 2 ]; then
-    conky --config="$HOME"/.config/conky/conky-left.conf
-elif xrandr --listactivemonitors | grep "HDMI" ; then
-    conky --config="$HOME"/.config/conky/conky-one-hdmi.conf
+host_computer=$(hostnamectl hostname)
+if [ "$host_computer" = "juanscr-acer-laptop" ]; then
+    conky_prefix="acer"
 else
-    conky --config="$HOME"/.config/conky/conky-one.conf
+    conky_prefix="work-laptop"
 fi
+
+conky \
+    --config="$HOME"/.config/conky/$conky_prefix-conky.conf \
+    -a top_right -x 30 -y 70 &
