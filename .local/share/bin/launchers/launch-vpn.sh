6019d9204db4b6937e4ca54e28e24476938819df
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2023-09-04 14:07:52 -0500

    Added non-sensitive script for launching vpn.

---
+++ .local/share/bin/launchers/launch-vpn.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo true;
+sudo openfortivpn -c /etc/openfortivpn/config &
+PID=$!
+sleep 5
+source ~/.local/share/bin/launchers/private-dns-variables.sh
+while true; do
+    sudo resolvectl dns ppp0 $dns_server \
+        && sudo resolvectl domain ppp0 ~. $domain_name \
+        && break
+done;
+unset dns_server
+unset domain_name
+while true; do sleep 60; done;
+kill $PID
