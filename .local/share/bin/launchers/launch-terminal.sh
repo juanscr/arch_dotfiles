137e71796863cc051e8b79474b17528f9dbd5101
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-06-19 11:19:19 -0500

    Launch terminal font size varying on rendering backend.

--- .local/share/bin/launchers/launch-terminal.sh
+++ .local/share/bin/launchers/launch-terminal.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-if [ `"$HOME"/.config/qtile/check_number_of_monitors.sh` == 1 ]; then
-    alacritty -o=font.size=9 "$@"
-else
-    alacritty "$@"
+if [[ `echo "$XDG_SESSION_TYPE"` == "x11" ]]; then
+    if [ `"$HOME"/.config/qtile/monitor_scripts/x11.sh` == 1 ]; then
+        alacritty -o=font.size=9 "$@"
+    else
+        alacritty "$@"
+    fi
+    exit 0;
 fi
+
+alacritty -o=font.size=16 "$@"
