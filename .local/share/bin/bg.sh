d79f9282d4fff988144f4a2ca1bba11150075f88
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-07-22 14:25:49 -0500

    Set background properly.

--- .local/share/bin/bg.sh
+++ .local/share/bin/bg.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+function setBg() {
+    if [[ `echo "$XDG_SESSION_TYPE"` == "x11" ]]; then
+        feh --no-fehbg --bg-scale $1
+        betterlockscreen -u $1
+        return;
+    fi
+    swaybg -i $1
+}
+
 if [ "$#" -lt 1 ]; then
     photo=$(find "$HOME"/.local/share/assets/wallpapers/ -name '*.jpg' | shuf -n 1)
 else
     photo=$1
 fi
 
-feh --no-fehbg --bg-scale "$photo"
-betterlockscreen -u "$photo"
+setBg $photo
