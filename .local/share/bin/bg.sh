dcec6c5116b16f06b77c941e8eab39b747e757b0
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-08-21 14:52:14 -0500

    Set hyprlock background through background script.

--- .local/share/bin/bg.sh
+++ .local/share/bin/bg.sh
@@ -6,7 +6,8 @@ function setBg() {
         betterlockscreen -u $1
         return;
     fi
-    swaybg -i $1
+    swaybg -i $1 &
+    mkdir -p ~/.local/share/hyprlock/ && ln -sf $1 ~/.local/share/hyprlock/bg.jpg
 }
 
 if [ "$#" -lt 1 ]; then
