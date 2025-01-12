c615b81cfb0835777353bfddbb564c1d45b17ca4
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2025-01-12 11:24:34 -0500

    Add toggle scheme for more desktop components.

--- .local/share/bin/toggle-scheme.sh
+++ .local/share/bin/toggle-scheme.sh
@@ -11,6 +11,11 @@ if [[ "$CURRENT_THEME" == "dark" ]]; then
     /usr/bin/nvim \
         --server /tmp/nvim.pipe \
         --remote-send ':lua SetColorScheme("solarized")<CR>'
+
+    # NWG Shell for all other apps
+    cp ~/.local/share/bin/assets/light-gsettings ~/.local/share/nwg-look/gsettings && \
+        nwg-look -a
+
 else
     export CURRENT_THEME="dark"
 
@@ -23,6 +28,10 @@ else
     /usr/bin/nvim \
         --server /tmp/nvim.pipe \
         --remote-send ':lua SetColorScheme("dracula")<CR>'
+
+    # NWG Shell for all other apps
+    cp ~/.local/share/bin/assets/dark-gsettings ~/.local/share/nwg-look/gsettings && \
+        nwg-look -a
 fi
 
 echo $CURRENT_THEME > ~/.local/share/.user_current_theme
