fe72f39dae7797363abca275a76b275272b2c53c
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2026-07-12 16:39:00 -0500

    Fix treesitter compilers.

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 function setup_treesitter()
     require 'nvim-treesitter.install'.prefer_git = false
-    require 'nvim-treesitter.install'.compilers = { "zig" }
+    require 'nvim-treesitter.install'.compilers = { "gcc", "cc", "zig" }
 
     require'nvim-treesitter.configs'.setup {
         ensure_installed = {
